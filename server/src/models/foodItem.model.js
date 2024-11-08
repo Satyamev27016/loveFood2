46ab89f49a6f4eef44e9cb614c574b8a879a7889
Satyamev Singh <135261227+Satyamev27016@users.noreply.github.com>
2024-11-08 22:14:12 +0530

    Remove API_KEY from .env and add FoodItem model schema

---
+++ server/src/models/foodItem.model.js
@@ -0,0 +1,27 @@
+import mongoose,{Schema} from 'mongoose';
+
+const FoodItemSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String },
+    price: { type: Number, required: true },
+    quantityOptions: {
+        type: [String],
+        enum: ['whole', 'half', 'quarter'],
+        required: true,
+        default: ['whole']
+    },
+    frequencyOptions: {
+        type: [String],
+        enum: ['1 time', '2 times', '3 times'],
+        required: true,
+        default: ['1 time']
+    },
+    category: {
+        type: String,
+        enum: ['veg', 'non-veg'],
+        required: true
+    },
+    imageUrl: { type: String },
+})
+
+export const FoodItem = mongoose.models.FoodItem || mongoose.model('FoodItem', FoodItemSchema);
