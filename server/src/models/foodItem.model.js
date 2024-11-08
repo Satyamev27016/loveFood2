d21c5e8943685a2c055fb15a50739d0b35a3b116
Satyamev Singh <135261227+Satyamev27016@users.noreply.github.com>
2024-11-08 23:04:38 +0530

    Refactor FoodItem model: comment out quantity and frequency options, rename imageUrl to image

--- server/src/models/foodItem.model.js
+++ server/src/models/foodItem.model.js
@@ -4,24 +4,24 @@ const FoodItemSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String },
     price: { type: Number, required: true },
-    quantityOptions: {
-        type: [String],
-        enum: ['whole', 'half', 'quarter'],
-        required: true,
-        default: ['whole']
-    },
-    frequencyOptions: {
-        type: [String],
-        enum: ['1 time', '2 times', '3 times'],
-        required: true,
-        default: ['1 time']
-    },
+    // quantityOptions: {
+    //     type: [String],
+    //     enum: ['whole', 'half', 'quarter'],
+    //     required: true,
+    //     default: ['whole']
+    // },
+    // frequencyOptions: {
+    //     type: [String],
+    //     enum: ['1 time', '2 times', '3 times'],
+    //     required: true,
+    //     default: ['1 time']
+    // },
     category: {
         type: String,
         enum: ['veg', 'non-veg'],
         required: true
     },
-    imageUrl: { type: String },
+    image: { type: String },
 })
 
 export const FoodItem = mongoose.models.FoodItem || mongoose.model('FoodItem', FoodItemSchema);
